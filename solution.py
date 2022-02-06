ba3acce5debce722b807b0de54bd99e0f539eabc
worcestershiresauce <99105013+worcestershiresauce@users.noreply.github.com>
2022-02-05 22:35:14 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def welcome_assignment_answers(question):
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "mtls"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
@@ -20,10 +20,10 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = "7"
+        answer = "5"
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = "4"
-    return(answer)
+        return(answer)
 # Complete all the questions.
 
 
